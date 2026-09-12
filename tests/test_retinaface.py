74656459c0e64046d52443686b37eb1d16d5ade6
xMaulana <muhammadwisnumaulana73@gmail.com>
2026-09-13 00:01:01 +0700

    chore: minimize test

--- tests/test_retinaface.py
+++ tests/test_retinaface.py
@@ -21,19 +21,19 @@ def test_retinaface_mobilenet_batched_inference():
     )
 
 
-def test_retinaface_resnet50_batched_inference():
-    model = FaceModelFactory.get_model("RETINAFACE_RESNET50", top_k=500)
+# def test_retinaface_resnet50_batched_inference():
+#     model = FaceModelFactory.get_model("RETINAFACE_RESNET50", top_k=500)
 
-    img1 = np.random.randint(0, 255, (480, 640, 3), dtype=np.uint8)
-    img2 = np.random.randint(0, 255, (720, 1280, 3), dtype=np.uint8)
+#     img1 = np.random.randint(0, 255, (480, 640, 3), dtype=np.uint8)
+#     img2 = np.random.randint(0, 255, (720, 1280, 3), dtype=np.uint8)
 
-    results = model.detect([img1, img2], return_dict=True)
+#     results = model.detect([img1, img2], return_dict=True)
 
-    assert len(results) == 2, "Should return results for two images"
+#     assert len(results) == 2, "Should return results for two images"
 
-    assert isinstance(results[0], list), (
-        "Result should be a list of dicts when return_dict=True"
-    )
-    assert isinstance(results[1], list), (
-        "Result should be a list of dicts when return_dict=True"
-    )
+#     assert isinstance(results[0], list), (
+#         "Result should be a list of dicts when return_dict=True"
+#     )
+#     assert isinstance(results[1], list), (
+#         "Result should be a list of dicts when return_dict=True"
+#     )
